d8bd98cc01046d539c15e13cf1e9fc1c077f876f
rodriguee <etienne.rodrigue89@gmail.com>
2015-07-16 21:30:33 -0400

    Add Mari_Channel_builder to Mari folder

--- mari/Mari_Channel_Builder.py
+++ mari/Mari_Channel_Builder.py
@@ -1,130 +1,127 @@
+#coding=utf-8
+
 import mari, os, hashlib
 import PythonQt.QtGui as gui
 import PythonQt.QtCore as core
+import sys
 
 
-
-def channel_builder():
-
-
-	#-----------------------------Boring stuff (AKA VARIABLES ET FONCTIONS)-----------------
-	dlg = gui.QDialog()
-	geo_list = mari.geo.list()
-	sel_obj = mari.geo.current()
-	chk_header = gui.QLabel("<strong>Channels To Build</strong>")
-	diff_chk = gui.QCheckBox("Diffuse")
-	bump_chk = gui.QCheckBox("Bump")
-	disp_chk = gui.QCheckBox("Displacement")
-	spec_chk = gui.QCheckBox("Specular")
-	norm_chk = gui.QCheckBox("Normal")
-	rough_chk = gui.QCheckBox("Roughness")
-	refl_chk = gui.QCheckBox("Reflection")
-	refr_chk = gui.QCheckBox("Refraction")
-	fres_chk = gui.QCheckBox("Fresnel")
-	mask_chk = gui.QCheckBox("Mask")	
-	# size = 
-	# depth = 
-	
-	#Fonction pour créer les checkbox et les assigner
-	def add_chk():
-		left_group_layout.addWidget(chk_header)	
-		left_group_layout.addWidget(diff_chk)
-		left_group_layout.addWidget(bump_chk)
-		left_group_layout.addWidget(disp_chk)
-		left_group_layout.addWidget(spec_chk)
-		left_group_layout.addWidget(norm_chk)
-		left_group_layout.addWidget(rough_chk)
-		left_group_layout.addWidget(refl_chk)
-		left_group_layout.addWidget(refr_chk)
-		left_group_layout.addWidget(fres_chk)
-		left_group_layout.addWidget(mask_chk)	
-
-	def select_all():
-		diff_chk.setChecked(1)
-		bump_chk.setChecked(1)
-		disp_chk.setChecked(1)
-		spec_chk.setChecked(1)
-		norm_chk.setChecked(1)
-		rough_chk.setChecked(1)
-		refl_chk.setChecked(1)
-		refr_chk.setChecked(1)
-		fres_chk.setChecked(1)
-		mask_chk.setChecked(1)
-
-	def select_none():
-		diff_chk.setChecked(0)
-		bump_chk.setChecked(0)
-		disp_chk.setChecked(0)
-		spec_chk.setChecked(0)
-		norm_chk.setChecked(0)
-		rough_chk.setChecked(0)
-		refl_chk.setChecked(0)
-		refr_chk.setChecked(0)
-		fres_chk.setChecked(0)
-		mask_chk.setChecked(0)
+class MariExporter(gui.QDialog):
+	def __init__(self):
+		super(MariExporter, self).__init__()
 
 
+		self.setWindowTitle("Channel Builder")
+		main_layout = gui.QHBoxLayout(self)
+
+		# Map Checkbox Layout
+		left_group = gui.QGroupBox(self)
+		self.channel_layout = gui.QGridLayout()
+		left_group.setLayout(self.channel_layout)
+		self.lbl = gui.QLabel("<b>Channels To Build</b>")
+		self.channel_layout.addWidget(self.lbl)
+		self.channel_layout.setColumnMinimumWidth(1, 5)
+
+		# Middle Layout
+		mid_group = gui.QGroupBox(self)
+		mid_group_layout = gui.QVBoxLayout(self)
+		mid_group.setLayout(mid_group_layout)
+
+		# Close Layout
+		self.right_group = gui.QGroupBox(self)
+		self.right_group_layout = gui.QVBoxLayout(self)
+		self.right_group.setLayout(self.right_group_layout)
+
+		# Add Layout to main
+		main_layout.addWidget(left_group)
+		main_layout.addWidget(mid_group)
+		main_layout.addWidget(self.right_group)
+
+		self.channel_builder()
+		self.bouton()
 
 
-
-	main_layout = gui.QHBoxLayout()
-
-	#Map Checkbox Layout
-	left_group = gui.QGroupBox()
-	left_group_layout = gui.QVBoxLayout()	
-	left_group.setLayout(left_group_layout)
-
-	#Middle Layout
-	mid_group = gui.QGroupBox()
-	mid_group_layout = gui.QVBoxLayout()	
-	mid_group.setLayout(mid_group_layout)
-
-	#Close Layout
-	right_group = gui.QGroupBox()
-	right_group_layout = gui.QVBoxLayout()	
-	right_group.setLayout(right_group_layout)
-
-	#Add Layout to main
-	main_layout.addWidget(left_group)
-	main_layout.addWidget(mid_group)
-	main_layout.addWidget(right_group)
+	def channel_builder(self):
 
 
+		#-----------------------------Boring stuff (AKA VARIABLES ET FONCTIONS)-----------------
 
+		geo_list = mari.geo.list()
+		sel_obj = mari.geo.current()
+		self.chk_dict = {}
+		self.chk_liste = []
+		diff_chk = gui.QCheckBox("Diffuse", self)
+		bump_chk = gui.QCheckBox("Bump", self)
+		disp_chk = gui.QCheckBox("Displacement", self)
+		spec_chk = gui.QCheckBox("Specular", self)
+		norm_chk = gui.QCheckBox("Normal", self)
+		roug_chk = gui.QCheckBox("Roughness", self)
+		refl_chk = gui.QCheckBox("Reflection", self)
+		refr_chk = gui.QCheckBox("Refraction", self)
+		fres_chk = gui.QCheckBox("Fresnel", self)
+		mask_chk = gui.QCheckBox("Mask", self)
+		self.chk_liste = [diff_chk, bump_chk, disp_chk, spec_chk, norm_chk, roug_chk, refl_chk, refr_chk, fres_chk, mask_chk]
+		self.chk_liste_name = ["diff_chk", "bump_chk", "disp_chk", "spec_chk", "norm_chk", "roug_chk", "refl_chk", "refr_chk", "fres_chk", "mask_chk"]
 
+	def bouton(self):
+		# Add Checkbox pour Map et Set to layout
+		self.add_chk()
 
-	#Add Checkbox pour Map et Set to layout	
-	add_chk()
+		# Select All & Select None
+		sel_all = gui.QPushButton("Select All")
+		sel_none = gui.QPushButton("Select None")
+		self.channel_layout.addWidget(sel_all)
+		self.channel_layout.addWidget(sel_none)
 
-	#Select All & Select None
-	sel_all = gui.QPushButton("Select All")
-	sel_none = gui.QPushButton("Select None")
-	left_group_layout.addWidget(sel_all)
-	left_group_layout.addWidget(sel_none)
+		sel_all.connect("clicked()", self.select_all)
+		sel_none.connect("clicked()", self.select_none)
 
-	sel_all.connect("clicked()", select_all)
-	sel_none.connect("clicked()", select_none)	
+		# Close button
+		close_btn = gui.QPushButton("Close")
+		self.right_group_layout.addWidget(close_btn)
+		close_btn.connect("clicked()", self.reject)
+		print self.chk_dict
 
+	def add_chk(self):
+		'''Fonction pour créer les checkbox et les assigner'''
+		temp = 0
+		for checkbox in self.chk_liste:
+			self.size_for_map = gui.QComboBox()
+			self.size_for_map.insertItem(0, "1024", )
+			self.size_for_map.insertItem(1, "2048", )
+			self.size_for_map.insertItem(2, "4096", )
+			self.size_for_map.insertItem(3, "8192", )
+			# self.size_for_map.insertItem(4, "16384", )    #PEUT-ETRE DISPONIBLE UN JOUR QUI SAIT ;_;
+			self.channel_layout.addWidget(self.chk_liste[temp])
+			self.channel_layout.addWidget(self.size_for_map)
+			temp_name = self.chk_liste_name[temp]
+			temp = temp + 1
+			self.chk_dict[temp_name] = self.size_for_map
 
+	def select_all(self):
+		'''Fonction pour selectionner tout'''
+		for checkbox in self.chk_liste:
+			checkbox.setChecked(1)
 
-	#Close button
-	close_btn = gui.QPushButton("Close")
-	right_group_layout.addWidget(close_btn)
-	close_btn.connect("clicked()", dlg.reject)
+	def select_none(self):
+		'''Fonction pour déselectionner tout'''
+		for checkbox in self.chk_liste:
+			checkbox.setChecked(0)
+
+	def check_state(self):
+		'''Fonction pour barrer les maps combobox'''
+		pass
 
 
 
 
 
 
+MainWindow = MariExporter()
+MainWindow.show()
 
-	dlg.setLayout(main_layout)
-	dlg.setWindowTitle("Channel Builder")
 
 
-	dlg.exec_()
-
-channel_builder()
 
 # colr	Diffuse color
 # bump	Bump
