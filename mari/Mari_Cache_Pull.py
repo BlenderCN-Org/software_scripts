d17a4b8dbe1dcc5873ced37bd8879a7e1ba9da26
rodriguee <etienne.rodrigue89@gmail.com>
2015-08-04 16:17:35 -0400

    Add Mari_Cache_Pull script to mari folder

--- mari/Mari_Cache_Pull.py
+++ mari/Mari_Cache_Pull.py
@@ -4,32 +4,29 @@ import os.path
 
 
 
-
-class Cache_Push():
+class Cache_Pull():
     def __init__(self):
-        self.path_to_cache = "H:\\Cache_2\\"        #Path to Source (Mari cache)
-        self.check_last_used()
-        self.push_folder()
+        self.path_to_cache_local = "H:\\Cache_3"
+        self.path_to_cache_server = "Z:\\Groupes-cours\\NAND999-A15-N01\\Nature\\tex"
+        # self.real_name_and_long_name = {}     Avoir un dict pour selectionner l'objet voulu [Cube : 74aed3[...] ]
 
-    def check_last_used(self):
-        self.last_used_directory = max([os.path.join(self.path_to_cache, d) for d in os.listdir(self.path_to_cache)], key=os.path.getmtime)
-        self.last_used_directory_name = self.last_used_directory.split("\\")[-1]        #Get Cache folder name (longue string)
 
-    def push_folder(self):
-        destination_folder = "Z:\\Groupes-cours\\NAND999-A15-N01\\Nature\\tex"
-        destPath = destination_folder + "\\"+ self.last_used_directory_name
 
-        for root, dirs, files in os.walk(self.last_used_directory):
 
-            #destination PATH
-            dest = destPath + root.replace(self.last_used_directory, '')
+    def pull_folder(self):
+        destPath = self.path_to_cache_local + "\\" + self.real_name_and_long_name[1]
 
-            #creer des directory si existe pas
+        for root, dirs, files in os.walk(self.path_to_cache_server):
+
+            # destination PATH
+            dest = destPath + root.replace(self.path_to_cache_server, '')
+
+            # creer des directory si existe pas
             if not os.path.isdir(dest):
                 os.mkdir(dest)
                 print 'Folder created at: ' + dest
 
-            #Boucler a travers les files
+            # Boucler a travers les files
             for f in files:
 
                 oldLoc = root + '\\' + f
@@ -49,12 +46,3 @@ class Cache_Push():
                         print 'File ' + f + ' copied.'
                     except IOError:
                         print 'File "' + f + '" already exists'
-
-
-
-
-
-
-
-
-Cache_Push()
